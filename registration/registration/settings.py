f18a76681967cf3f21335fcc3eb3b4e57e18db84
MaryChrispinaNjuguna <chrispinamary@gmail.com>
2024-06-11 09:37:05 +0300

    settings

--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,7 +122,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, "static"),
 ]
 
 
