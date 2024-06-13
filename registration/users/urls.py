86803ac63a92ae4b966968b4611fcd0c1c4058b7
MaryChrispinaNjuguna <chrispinamary@gmail.com>
2024-06-13 23:08:25 +0300

    nav bar

--- registration/users/urls.py
+++ registration/users/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.home)
+    path('', views.home, name='home'),
+     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
 ]
--- registration/users/views.py
+++ registration/users/views.py
@@ -4,4 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'users/home.html')
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def register_view(request):
+    return render(request, 'register.html')
     
