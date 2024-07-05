05b057e5d92b599944890774721133ed8c67aeb8
MaryChrispinaNjuguna <chrispinamary@gmail.com>
2024-07-05 19:56:08 +0300

    logout works

--- registration/users/urls.py
+++ registration/users/urls.py
@@ -1,13 +1,14 @@
+# urls.py
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_view
+from django.contrib.auth import views as auth_views
 
-urlpatterns=[
+urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('profile/', views.profile, name='profile'),
     path('contact/', views.contact, name='contact'),
-    path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', views.register_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
